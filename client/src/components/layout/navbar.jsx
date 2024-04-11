8923947cad114fd213151fdd56306b73e21cab7c
kevkimbu <kevkim@bu.edu>
2024-04-11 02:27:47 -0400

    Style navigation bar

--- client/src/components/layout/navbar.jsx
+++ client/src/components/layout/navbar.jsx
@@ -1,17 +1,19 @@
 import {NavLink} from 'react-router-dom';
 
+import logo from '../../assets/logo.png'
+
 export default function Navbar() {
   return (
-    <div className="flex flex-row justify-between items-center p-4 bg-black w-full">
-      <h3 className="text-white">Logo</h3>
+    <div className="flex justify-between items-center p-4 bg-black w-full">
+      <img src={logo} alt="logo" className="w-1/12 pl-3" />
       <div className="">
-        <NavLink className="hover:text-sky-500 text-white font-bold py-2 px-6 rounded" to={`/`}>
+        <NavLink className="hover:text-sky-500 font-roboto text-white text-xl py-6 px-6 rounded" to={`/`}>
           Home
         </NavLink>
-        <NavLink className="hover:text-sky-500 text-white font-bold py-2 px-6 rounded" to={`/jobs`}>
+        <NavLink className="hover:text-sky-500 font-roboto text-white text-xl py-6 px-6 rounded" to={`/jobs`}>
           Jobs
         </NavLink>
-        <NavLink className="hover:text-sky-500 text-white font-bold py-2 px-6 rounded" to={`/gigs`}>
+        <NavLink className="hover:text-sky-500 font-roboto text-white text-xl py-6 px-6 rounded" to={`/gigs`}>
           Gigs
         </NavLink>
       </div>
