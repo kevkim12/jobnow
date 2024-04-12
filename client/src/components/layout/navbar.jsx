20240cd95b62aa09a5cf4b60f91812870a3c092c
Lawrence Li <lawrenceli3202@gmail.com>
2024-04-11 23:37:04 -0400

    Add styling to nav bar

--- client/src/components/layout/navbar.jsx
+++ client/src/components/layout/navbar.jsx
@@ -1,19 +1,19 @@
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.png';
 
 export default function Navbar() {
   return (
     <div className="flex justify-between items-center p-4 bg-black w-full">
       <img src={logo} alt="logo" className="w-1/12 pl-3" />
       <div className="">
-        <NavLink className="hover:text-theme font-roboto text-white text-xl py-6 px-6 rounded" to={`/`}>
+        <NavLink className="hover:text-theme font-roboto text-white text-xl py-6 px-6 rounded hover:underline hover:underline-offset-8 hover:bg-slate-200" to={`/`}>
           Home
         </NavLink>
-        <NavLink className="hover:text-theme font-roboto text-white text-xl py-6 px-6 rounded" to={`/gigs`}>
+        <NavLink className="hover:text-theme font-roboto text-white text-xl py-6 px-6 rounded hover:underline hover:underline-offset-8 hover:bg-slate-200" to={`/gigs`}>
           Gigs
         </NavLink>
-        <NavLink className="font-roboto text-white text-xl py-2 px-4 rounded bg-theme ml-2.5" to={`/login`}>
+        <NavLink className="font-roboto text-white text-xl py-2 px-4 rounded bg-theme ml-2.5 hover:text-theme hover:bg-slate-200" to={`/login`}>
           Login
         </NavLink>
       </div>
