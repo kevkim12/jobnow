fdad5be416f5fbb3a06017a609ae0cd2de0e0554
kevkimbu <kevkim@bu.edu>
2024-04-12 21:24:06 -0400

    Change navigation buttons theme

--- client/src/components/layout/navbar.jsx
+++ client/src/components/layout/navbar.jsx
@@ -7,10 +7,10 @@ export default function Navbar() {
     <div className="flex justify-between items-center p-4 bg-black w-full">
       <img src={logo} alt="logo" className="w-1/12 pl-3" />
       <div className="">
-        <NavLink className="hover:text-theme font-roboto text-white text-xl py-6 px-6 rounded hover:underline hover:underline-offset-8 hover:bg-slate-200" to={`/`}>
+        <NavLink className="hover:text-theme font-roboto text-white text-xl py-6 px-6 rounded" to={`/`}>
           Home
         </NavLink>
-        <NavLink className="hover:text-theme font-roboto text-white text-xl py-6 px-6 rounded hover:underline hover:underline-offset-8 hover:bg-slate-200" to={`/gigs`}>
+        <NavLink className="hover:text-theme font-roboto text-white text-xl py-6 px-6 rounded" to={`/gigs`}>
           Gigs
         </NavLink>
         <NavLink className="font-roboto text-white text-xl py-2 px-4 rounded bg-theme ml-2.5 hover:text-theme hover:bg-slate-200" to={`/login`}>
