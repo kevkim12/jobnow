78764b23ebb3273103c737aa1a353d09cf3c28b6
Lawrence Li <lawrenceli3202@gmail.com>
2024-04-18 23:24:27 -0400

    Add validation errors and requirements for signup

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,6 +1,7 @@
 import { UsergroupAddOutlined } from '@ant-design/icons';
 import { Button, Card, Checkbox, Form, Input } from 'antd';
 import { NavLink } from 'react-router-dom';
+// import { validateEmail, validatePassword } from '../utility/validation';
 
 const onFinish = (values) => {
   console.log('Success:', values);
@@ -13,7 +14,7 @@ export default function Login() {
   return (
     <div className='h-screen flex flex-col justify-center items-center w-full'>
       <Card className='w-1/3 border-black rounded-xl shadow-2xl'>
-        <h1 className='text-center text-3xl mb-5 text-[#455a64]'>Login</h1>
+        <h1 className='text-center text-3xl mb-5 text-black'>Login</h1>
         <Form
           name="basic"
           initialValues={{ remember: true }}
@@ -29,7 +30,16 @@ export default function Login() {
               {
                 required: true,
                 message: 'An email address is required.'
-              },]}
+              },
+              // () => ({
+              //   validator(_, value) {
+              //     if (!value || validateEmail(value)) {
+              //       return Promise.resolve();
+              //     }
+              //     return Promise.reject(new Error('Invalid email format'));
+              //   },
+              // }),
+            ]}
           >
             <Input placeholder="Email" />
           </Form.Item>
@@ -40,7 +50,16 @@ export default function Login() {
               {
                 required: true,
                 message: 'A password is required.'
-              },]}
+              },
+              // () => ({
+              //   validator(_, value) {
+              //     if (!value || validatePassword(value)) {
+              //       return Promise.resolve();
+              //     }
+              //     return Promise.reject(new Error('Password must be at least 8 characters long and contain at least 1 digit, 1 uppercase, and 1 lowercase character'));
+              //   },
+              // }),
+            ]}
           >
             <Input.Password placeholder="Password" />
           </Form.Item>
