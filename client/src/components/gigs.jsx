f35fd5c8a2b9fc51be97cd9be889bd93392685e1
kevkimbu <kevkim@bu.edu>
2024-04-25 03:52:38 -0400

    Hide bookmark button from users who are not logged in.

--- client/src/components/gigs.jsx
+++ client/src/components/gigs.jsx
@@ -15,7 +15,7 @@ export default function Gigs() {
   const [filteredPosts, setFilteredPosts] = useState([]);
   const [price, setPrice] = useState("");
   const [error, setError] = useState('');
-  const { userId } = useAuth();
+  const { userId, isLoggedIn } = useAuth();
   const [bookmarkedPosts, setBookmarkedPosts] = useState({});
 
   const fetchBookmarkStatus = async (post) => {
@@ -224,26 +224,28 @@ export default function Gigs() {
           <Card key={index} className="mb-4 border-black">
             <div className="flex justify-between items-center mb-2">
               <h1 className="text-2xl font-semibold">Name: {post.name}</h1>
+              {isLoggedIn && (
               <button id={post.id} className=" bg-theme text-white p-2 rounded-md" onClick={() => bookmarkPost(post.id)}>
               {bookmarkedPosts[post.id] ? (
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24 "
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6">
+                <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clipRule="evenodd" />
                 </svg>
               ) : (
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6">
-                  <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clipRule="evenodd" />
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24 "
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" /> 
                 </svg> )}
               </button>
+              )}
             </div>
             <Divider></Divider>
             <p className="text-xl mb-2">Subject: {post.subject}</p>
